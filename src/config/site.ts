7405100c52ac06e374d684096abb8359aa35edbd
moira <moirachelseyburbos@gmail.com>
2026-05-25 17:07:05 +0800

    Add phone number

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,6 +2,5 @@ export const SITE_CONTACT = {
   facebookPage: '@coniebakes',
   facebookUrl: 'https://facebook.com/coniebakes',
   email: 'coniebakes@gmail.com',
-  /** Replace with your phone number when ready */
   phone: '0925 844 8876',
 };
