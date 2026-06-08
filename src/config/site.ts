85159de6662d30a6b01c8b25dcd8858f803cb338
moira <moirachelseyburbos@gmail.com>
2026-06-08 15:25:06 +0800

    Add Instagram link to footer and expand grid layout

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,8 @@
 export const SITE_CONTACT = {
   facebookPage: '@coniebakes',
   facebookUrl: 'https://facebook.com/coniebakes',
+  instagramUrl: 'https://www.instagram.com/coniebakes',
+  instagramPage: '@coniebakes',
   email: 'coniebakes@gmail.com',
   phone: '0925 844 8876',
 };
